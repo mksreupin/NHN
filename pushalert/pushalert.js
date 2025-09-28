86169e8a8f0806482d904ece45c41da82b27998d
MK Guru <64196459+mksreupin@users.noreply.github.com>
2025-09-28 14:20:51 +0530

    Update pushalert.js

--- pushalert/pushalert.js
+++ pushalert/pushalert.js
@@ -3,7 +3,7 @@
         (function(d, t) {
                 var g = d.createElement(t),
                 s = d.getElementsByTagName(t)[0];
-                g.src = "integrate_96d7c7b27d00ce7aa1a20df0620918ea.js";
+                g.src = "https://raw.githubusercontent.com/MKSREUPIN/NHN/refs/heads/main/pushalert/integrate_96d7c7b27d00ce7aa1a20df0620918ea.js";
                 s.parentNode.insertBefore(g, s);
         }(document, "script"));
 </script>
